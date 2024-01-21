fd18db1c00ae2f91af7b6e7d814e5b8854e15dc7
Julia Erbis <iuliia.erbis@gmail.com>
2024-01-21 17:43:35 +0100

    Latest Projects section erstellt und content hinzugefügt

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,80 @@
+import "./Projects.css";
+import Frame from "../images/Frame.png";
+
 export const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="Projects">
+      <div className="heading">
+        <h1>Latest Projects</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, quidem
+          autem! Eos fugiat alias labore.
+        </p>
+      </div>
+      {/* Card-01 */}
+      <div className="project-card">
+        <div className="project-content">
+          <div className="project-frame">
+            <img src={Frame} alt="Frame" />
+          </div>
+
+          <div className="project-text">
+            <h2>Web Development</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Blanditiis quae ipsa soluta itaque. In, debitis!
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Card-02 */}
+      <div className="project-card">
+        <div className="project-content">
+          <div className="project-frame">
+            <img src={Frame} alt="Frame" />
+          </div>
+
+          <div className="project-text">
+            <h2>Web Development</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Blanditiis quae ipsa soluta itaque. In, debitis!
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Card-03 */}
+      <div className="project-card">
+        <div className="project-content">
+          <div className="project-frame">
+            <img src={Frame} alt="Frame" />
+          </div>
+
+          <div className="project-text">
+            <h2>Web Development</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Blanditiis quae ipsa soluta itaque. In, debitis!
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Card-04 */}
+      <div className="project-card">
+        <div className="project-content">
+          <div className="project-frame">
+            <img src={Frame} alt="Frame" />
+          </div>
+
+          <div className="project-text">
+            <h2>Web Development</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Blanditiis quae ipsa soluta itaque. In, debitis!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
