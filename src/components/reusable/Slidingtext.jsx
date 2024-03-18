ac1bbeb535c9a41101c59b61be7884743df8b3ed
Julia Erbis <iuliia.erbis@gmail.com>
2024-03-18 14:45:18 +0100

    Slidingtext

--- src/components/reusable/Slidingtext.jsx
+++ src/components/reusable/Slidingtext.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import "./Slidingtext.css";
 
 export const Slidingtext = () => {
-  return <div>Slidingtext</div>;
+  return (
+    <ul className="Slidingtext">
+      <li>Bootstrap</li>
+      <li>Node.js</li>
+      <li>Github</li>
+      <li>React</li>
+      <li>Tailwind</li>
+      <li>Html</li>
+    </ul>
+  );
 };
