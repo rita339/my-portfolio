01408dfa92fb0e29a58f52b431dd6409e2e0e247
Julia Erbis <iuliia.erbis@gmail.com>
2024-03-18 14:26:04 +0100

    React Router und allgemeine Einstellungen

---
+++ src/components/reusable/Slidingtext.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Slidingtext = () => {
+  return <div>Slidingtext</div>;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Home } from "./components/Home.jsx";
+import { Projects } from "./components/Projects.jsx";
+import { Resume } from "./components/Resume.jsx";
+import { Contact } from "./components/Contact.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Skills } from "./components/Skills.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "resume",
+        element: <Resume />,
+      },
+      {
+        path: "skills",
+        element: <Skills />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
